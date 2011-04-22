9b00f1106111be141fe1d92e37cc2954917502e4
falconlux <luxiang@dislab.nju.edu.cn>
2011-04-22 18:23:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lilystock::Application.config.secret_token = '647fb81b7a70f0fba8dc9e898588f57d00ca924e1f24a9333a047ec3f1c98354cae536c57d9c1a1f04543c5ac546f4db598536e5c6b1d32c8c52a82084081101'
